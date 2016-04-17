3d339f2367c42f6de67f5b34e43d48307e04d69a
fde001 <fde@mail.ru>
2016-04-17 09:14:33 +0300

    Track-me.pro connection proto

--- IntegrationServer/ActivityActions/TrackMeProConnector.cs
+++ IntegrationServer/ActivityActions/TrackMeProConnector.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using IntegrationServer.TrackMeProControl;
 
 namespace IntegrationServer.ActivityActions
 {
@@ -9,7 +10,8 @@ namespace IntegrationServer.ActivityActions
     {
         public static void SendResult(int controlPointId, string startNumber, DateTime time)
         {
-
+            var service = new ControlPointClient();
+            service.SendTimeControl(controlPointId, startNumber, time);
         }
     }
 }
